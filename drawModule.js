78f3e3dc79e1f8458bdffcf38b3ce486856bf76f
velikovmartin <velikovmartin@yahoo.com>
2016-10-11 21:32:02 +0300

    drawModule update

--- drawModule.js
+++ drawModule.js
@@ -1,4 +1,20 @@
 let drawModule = (function () {
-
+    let bodySnake = function (x, y) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+        ctx.strokeStyle = 'darkgreen';
+        ctx.strokeRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+    }
+    let pizza = function(x, y) {
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize,snakeSize)
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
+    }
+    let scoreText = function () {
+        let score_text = "Score: " + score;
+        ctx.fillStyle = 'blue';
+        ctx.fillText(score_text, 145, h-5);
+    }
 	
 }());
