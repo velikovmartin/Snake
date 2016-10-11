69e4ef575586b4b19b12cb2432d91ef50addbb03
Teodora.Nozharova <tedinoji@gmail.com>
2016-10-11 21:44:52 +0300

    added main function and initialization function.

--- drawModule.js
+++ drawModule.js
@@ -1,4 +1,88 @@
 let drawModule = (function () {
+    let bodySnake = function (x, y) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+        ctx.strokeStyle = 'darkgreen';
+        ctx.strokeRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+    }
+    let pizza = function(x, y) {
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize,snakeSize);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
+    }
+    let scoreText = function () {
+        let score_text = "Score: " + score;
+        ctx.fillStyle = 'blue';
+        ctx.fillText(score_text, 145, h-5);
+    }
 
-	
+    var paint = function () {
+        ctx.fillStyle = 'lightgrey';
+        ctx.fillRect(0, 0, w, h);
+
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(0, 0, w, h);
+
+        btn.setAttribute('disabled', true);
+
+        var snakeX = snake[0].x;
+        var snakeY = snake[0].y;
+
+        if (direction == 'right') {
+            snakeX++;
+        } else if (direction == 'left') {
+            snakeX--;
+        } else if (direction == 'up') {
+            snakeY--;
+        } else if (direction == 'down') {
+            snakeY++;
+        }
+
+
+        if (snakeX == -1 || snakeX == w / snakeSize || snakeY == -1 || snakeY == h / snakeSize || check_collision(snakeX, snakeY, snake)) {
+
+            btn.removeAttribute('disabled', true);
+
+            ctx.clearRect(0, 0, w, h);
+            gameloop = clearInterval(gameloop);
+            return;
+        }
+
+        if (snakeX == food.x && snakeY == food.y) {
+            var tail = {
+                x: snakeX,
+                y: snakeY
+            };
+
+            score++;
+            createFood();
+        } else {
+            var tail = snake.pop();
+            tail.x = snakeX;
+            tail.y = snakeY;
+        }
+
+        snake.unshift(tail);
+
+        for (var i = 0; i < snake.length; i++) {
+            bodySnake(snake[i].x, snake[i].y);
+        }
+
+        pizza(food.x, food.y);
+
+        scoreText();
+    }
+
+    var init = function () {
+        direction = 'down';
+        drawSnake();
+        createFood();
+        gameloop = setInterval(paint, 80);
+    }
+
+    return {
+        init: init
+    };
+
 }());
