d8ef9270f98d6a648864ef4e60ba6db58a481082
sstoqnov <s.stoqnov87@gmail.com>
2016-10-12 21:06:17 +0300

    Допълнение  към контролите
    направих ги и за лява ръка

--- app.js
+++ app.js
@@ -39,6 +39,34 @@
                     console.log('down');
                 }
                 break;
+
+            case 87:
+                if (direction !='down'){
+                    direction = 'up';
+                    console.log('up')
+                }
+                break;
+
+            case 83:
+                if (direction !='up'){
+                    direction = 'down';
+                    console.log('down')
+                }
+                break;
+
+            case 65:
+                if (direction !='right'){
+                    direction = 'left';
+                    console.log('left')
+                }
+                break;
+
+            case 68:
+                if (direction !='left'){
+                    direction = 'right';
+                    console.log('right')
+                }
+                break;
         }
     }
 })(window, document, drawModule);
